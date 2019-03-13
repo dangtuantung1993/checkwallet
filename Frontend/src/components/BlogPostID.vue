e357832beb45f03c4f78ade72cd9346e378710c4
dangtuantung1993 <dangtuantung150993@gmail.com>
2019-03-13 20:47:54 +0700

    pramaid

---
+++ Frontend/src/components/BlogPostID.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <p>title: {{ blogPosts.title }}</p>
+        <p>content: {{ blogPosts.content }}</p>
+    </div>
+</template>
+
+<script>
+    import {listBlogpost} from "../APIs/blogPostsAPI";
+
+    export default {
+        name: "BlogPostID",
+        data() {
+            return {
+                blogPosts: {}
+            }
+        },
+
+        created() {
+            this.fetchData()
+        },
+
+        watch: {
+            '$route': 'fetchData'
+        },
+
+        methods: {
+            fetchData: async function () {
+                let blogposts = await listBlogpost()
+                this.blogPosts = blogposts[this.$route.params.id]
+                console.log('hehe',blogPosts.params.id)
+            }
+        }
+
+    }
+</script>
+
+<style scoped>
+
+</style>
