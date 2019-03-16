88f9bfe34570b343e538e05a237b9280dd4726c3
dangtuantung1993 <36393757+dangtuantung1993@users.noreply.github.com>
2019-03-17 00:35:51 +0700

    Update database.js

--- Backend/database/database.js
+++ Backend/database/database.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose')
 //Kết nối CSDL MongoDB
 const connectDatabase = async () => {
     try {
-        let uri = 'mongodb://dangtuantung1993:vannhuthe1@127.0.0.1:27018/CMTproject'
+        let uri = 'mongodb://dangtuantung1993:vannhuthe1@webversion1-shard-00-00-8rtop.mongodb.net:27017,webversion1-shard-00-01-8rtop.mongodb.net:27017,webversion1-shard-00-02-8rtop.mongodb.net:27017/test?ssl=true&replicaSet=Webversion1-shard-0&authSource=admin&retryWrites=true'
         let options = {
             connectTimeoutMS: 10000,// 10 giây
             useNewUrlParser: true,
